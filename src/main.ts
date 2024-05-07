50d951652df60225dd03e56748487a79b8e0fb7b
SP-Geno <140417409+SP-Geno@users.noreply.github.com>
2024-05-07 15:17:59 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-	WA.controls.disableMouse();
 
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
